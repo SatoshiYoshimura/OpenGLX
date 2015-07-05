19442aaf968e9288fef3cef7445f7955217ad5d7
satoshiyoshimura <satoshi.yoshimura.so@gmail.com>
2015-07-05 17:29:54 +0900

    Initial Commit

---
+++ OpenGLOSX/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  OpenGLOSX
+//
+//  Created by 吉村 智志 on 7/5/15.
+//  Copyright (c) 2015 吉村 智志. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
---
+++ OpenGLOSX/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  OpenGLOSX
+//
+//  Created by 吉村 智志 on 7/5/15.
+//  Copyright (c) 2015 吉村 智志. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
